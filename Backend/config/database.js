af4cad054637132f71706bfc45a254c671bd84ae
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2024-07-03 23:22:34 +0530

    ssl-tl added

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,12 +3,12 @@ const pg = require("pg");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: pg,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
