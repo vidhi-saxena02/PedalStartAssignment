3d6e75f6e85d989938108aaaa06e334bdf4c038f
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2024-07-03 23:47:54 +0530

    Api change

--- Frontend/src/Helper/Apis.js
+++ Frontend/src/Helper/Apis.js
@@ -1,31 +1,27 @@
 import axios from "axios";
 
+const api = "https://pedalstartassignment.onrender.com/api/tasks";
 export const fetchTask = async () => {
-  const { data } = await axios.get("http://localhost:3000/api/tasks");
+  const { data } = await axios.get(api);
   return data;
 };
 
 export const postTask = async (task) => {
-  const { data } = await axios.post("http://localhost:3000/api/tasks", task);
+  const { data } = await axios.post(api, task);
   return data;
 };
 
 export const deleteTaskStatus = async (id) => {
-  const { data } = await axios.delete(`http://localhost:3000/api/tasks/${id}`);
+  const { data } = await axios.delete(`${api}/${id}`);
   return data;
 };
 
 export const editTaskData = async (task) => {
-  const { data } = await axios.put(
-    `http://localhost:3000/api/tasks/${task.id}`,
-    task.tasks
-  );
+  const { data } = await axios.put(`${api}/${task.id}`, task.tasks);
   return data;
 };
 
 export const markTaskCompeted = async (id) => {
-  const { data } = await axios.put(
-    `http://localhost:3000/api/tasks/${id}/completed`
-  );
+  const { data } = await axios.put(`${api}/${id}/completed`);
   return data;
 };
